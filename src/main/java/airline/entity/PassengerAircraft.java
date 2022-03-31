517b1a8903be94b34b43eeabadb90b3c6861c0be
yslsml <mmf.antonovam@gmail.com>
2022-03-31 19:10:34 +0300

    feat: add PassengerAircraft class

---
+++ src/main/java/airline/entity/PassengerAircraft.java
@@ -0,0 +1,25 @@
+package airline.entity;
+
+public class PassengerAircraft extends Aircraft{
+    private int numberOfSeats;
+
+    public PassengerAircraft(long aircraftId, String name, double flightRange, double fuelConsumption, int numberOfSeats) {
+        super(aircraftId, name, flightRange, fuelConsumption);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPassengerAircraft {" +
+                "\n id = " + getAircraftId() +
+                "\n name = " + getName() +
+                "\n flight range = " + getFlightRange() +
+                "\n fuel consumption = " + getFuelConsumption() +
+                "\n numberOfSeats = " + numberOfSeats +
+                "\n}";
+    }
+}
