e43f4bda6caf6aa48e21eda57bab99013669dee2
yslsml <mmf.antonovam@gmail.com>
2022-04-04 12:38:06 +0300

    feat: add utils classes

---
+++ src/main/java/airline/util/AircraftTags.java
@@ -0,0 +1,13 @@
+package airline.util;
+
+// имена данных в строке исходного файла
+public class AircraftTags {
+    public static final String ID = "ID=";
+    public static final String NAME = "NAME=";
+    public static final String TYPE = "TYPE=";
+    public static final String FLIGHT_RANGE = "FLIGHT_RANGE=";
+    public static final String FUEL_CONSUMPTION = "FUEL_CONSUMPTION=";
+    public static final String LOAD_CAPACITY = "LOAD_CAPACITY=";
+    public static final String NUMBER_OF_BOMBS = "NUMBER_OF_BOMBS=";
+    public static final String NUMBER_OF_SEATS = "NUMBER_OF_SEATS=";
+}
---
+++ src/main/java/airline/util/AircraftType.java
@@ -0,0 +1,8 @@
+package airline.util;
+
+// типы воздушных судн в файле
+public enum AircraftType {
+    CARGO_AIRCRAFT,
+    MILITARY_AIRCRAFT,
+    PASSENGER_AIRCRAFT
+}
