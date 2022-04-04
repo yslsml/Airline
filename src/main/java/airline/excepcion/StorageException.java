43fdd88f540b6df3e7e4ccc41b3ea6e66a7e6917
yslsml <mmf.antonovam@gmail.com>
2022-04-04 12:20:38 +0300

    feat: add StorageException class

---
+++ src/main/java/airline/excepcion/StorageException.java
@@ -0,0 +1,22 @@
+package airline.excepcion;
+
+public class StorageException extends Exception{
+    public StorageException() {
+    }
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StorageException(Throwable cause) {
+        super(cause);
+    }
+
+    public StorageException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
